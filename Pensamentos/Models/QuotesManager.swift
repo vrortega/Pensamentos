777c5821eeaa4c447529988d1d2d4151eac9f931
vrortega <enigmadanceteam@gmail.com>
2024-06-05 15:21:31 -0300

    feat: add JSON data for quotes

--- Pensamentos/Models/QuotesManager.swift
+++ Pensamentos/Models/QuotesManager.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class QuotesManager {
-    
+   let quotes: [Quote]
+   
+   init() {
+       // Garante que o fileURL não seja nulo
+       guard let fileURL = Bundle.main.url(forResource: "quotes", withExtension: "json") else {
+           fatalError("Arquivo quotes.json não encontrado.")
+       }
+       
+       do {
+           // Inicializa jsonData
+           let jsonData = try Data(contentsOf: fileURL)
+           let jsonDecoder = JSONDecoder()
+           
+           // Decodifica as quotes
+           quotes = try jsonDecoder.decode([Quote].self, from: jsonData)
+       } catch {
+           // Lida com qualquer erro durante a decodificação dos dados
+           fatalError("Falha ao carregar ou decodificar quotes.json: \(error)")
+       }
+   }
 }
