0a4ccbe76f091ef4b2c981bdc6a559fd47d6118d
vrortega <enigmadanceteam@gmail.com>
2024-06-06 09:51:33 -0300

    feat: show a quote at an interval of 8 seconds

--- Pensamentos/Controllers/QuotesViewController.swift
+++ Pensamentos/Controllers/QuotesViewController.swift
@@ -15,6 +15,7 @@ class QuotesViewController: UIViewController {
     @IBOutlet weak var authorLb: UILabel!
     
     let quotesManger = QuotesManager()
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,30 @@ class QuotesViewController: UIViewController {
     }
     
     func prepareQuote() {
-        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 8.0, repeats: true, block: { (timer) in
+            self.showRandomQuote()
+        })
+        showRandomQuote()
     }
-
+    
+    func showRandomQuote() {
+        let quote = quotesManger.getRandomQuote()
+        quoteLb.text = quote.quote
+        authorLb.text = quote.author
+        imageIv.image = UIImage(named: quote.image)
+        imageBg.image = imageIv.image
+        
+        quoteLb.alpha = 0.0
+        authorLb.alpha = 0.0
+        imageIv.alpha = 0.0
+        imageBg.alpha = 0.0
+        
+        UIView.animate(withDuration: 2.5){
+            self.quoteLb.alpha = 1.0
+            self.authorLb.alpha = 1.0
+            self.imageIv.alpha = 1.0
+            self.imageBg.alpha = 0.25
+        }
+    }
 }
